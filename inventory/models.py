57e3c5ee0302619e4a232997502bf7869ee507c7
6iruk <biruktarega@gmail.com>
2025-02-12 13:22:30 +0300

    Update database design

--- inventory/models.py
+++ inventory/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 USER_ROLES = (
     ('ADMIN', 'Admin'),
     ('MANAGER', 'Manager'),
+    ('CASHIER', 'Cashier'),
 )
 
 EMPLOYEE_TYPES = [
@@ -17,15 +18,6 @@ EMPLOYEE_TYPES = [
 DEPARTMENTS = [
     ('CHAIR', 'Chair'),
     ('TABLE', 'Table'),
-]
-
-REQUEST_STATUS = [
-    ('PENDING', 'Pending'),
-    ('ACCEPTED', 'Accepted'),
-    ('PROCESSING', 'Processing'),
-    ('IN_TRANSIT', 'In Transit'),
-    ('DELIVERED', 'Delivered'),
-    ('CANCELLED', 'Cancelled')
 ]
 
 phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
@@ -55,17 +47,15 @@ class Employee(models.Model):
     notes = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
-class Usage(models.Model):
+class WorkOrder(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    request_date = models.DateTimeField()
+    order_date = models.DateTimeField()
     notes = models.TextField(blank=True, null=True)
 
-class Procurement(models.Model):
+class Sales(models.Model):
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    request_date = models.DateTimeField()
-    delivery_date = models.DateTimeField()
-    status = models.CharField(max_length=20, choices=REQUEST_STATUS, default='PENDING')
+    sales_date = models.DateTimeField()
     notes = models.TextField(blank=True, null=True)
