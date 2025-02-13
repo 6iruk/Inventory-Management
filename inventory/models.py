e16302135f6aa191b8d32aba5906db13625aea82
6iruk <biruktarega@gmail.com>
2025-02-13 10:49:32 +0300

    Add __str__ functions

--- inventory/models.py
+++ inventory/models.py
@@ -27,6 +27,8 @@ class Account(models.Model):
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True, null=True, unique=True)
     role = models.CharField(max_length=20, choices=USER_ROLES)
     
+    def __str__(self):
+        return self.phone_number
 
 class Material(models.Model):
     name = models.CharField(max_length=100)
@@ -36,6 +38,9 @@ class Material(models.Model):
     stock_quantity = models.PositiveIntegerField(default=0)
     unit_price = models.DecimalField(decimal_places=2, max_digits=10)
 
+    def __str__(self):
+        return self.material_code
+    
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20, unique=True)
@@ -45,6 +50,9 @@ class Employee(models.Model):
     notes = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.phone_number
+    
 class WorkOrder(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
@@ -52,8 +60,14 @@ class WorkOrder(models.Model):
     order_date = models.DateTimeField()
     notes = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.material + str(self.quantity)
+    
 class Sales(models.Model):
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     sales_date = models.DateTimeField()
     notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.material + str(self.quantity)
